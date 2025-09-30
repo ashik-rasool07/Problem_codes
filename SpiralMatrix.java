1d5f327649346514631b8c1207e4582c73abb889
ashik-rasool07 <ashikrasool486486@gmail.com>
2025-09-30 23:51:36 +0530

    edit

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class SpiralMatrix {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         // Step 1: Read N
